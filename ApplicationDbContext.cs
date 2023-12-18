af0e0f9841a45c08cfb008f4a3bde0587af20785
FioQG <152979836+FioQG@users.noreply.github.com>
2023-12-17 23:54:29 -0500

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Controllers.Entidades;
+
+namespace WebApiAutores
+{
+    public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Autor> Autores { get; set; }
+
+    }
+}
