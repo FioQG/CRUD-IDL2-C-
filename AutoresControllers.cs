bf0ac787a3d48fc817110f7f01f256712b6b71e6
FioQG <152979836+FioQG@users.noreply.github.com>
2023-12-18 01:21:55 -0500

    Add files via upload

---
+++ AutoresControllers.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Controllers.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresControllers(ApplicationDbContext context) : ControllerBase
+    {
+        private readonly ApplicationDbContext context = context;
+
+        [HttpGet]
+        public async Task<List<Autor>> Get ()
+        {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ Entidades/Autor.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiAutores.Controllers.Entidades
+{
+    public class Autor
+    { 
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+    }
+}
